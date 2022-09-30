9a52bbac16110a9a4e7936d2621b56eeb39f72b2
FRex <blazej.roszkowski@gmail.com>
2022-10-01 00:00:47 +0200

    fixes for python 27

--- csoinfo.py
+++ csoinfo.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import struct
 import sys
 import os
 
@@ -7,12 +8,12 @@ def pretty_print_filesize(fsize):
         return '{} bytes'.format(fsize)
 
     if fsize < 1024 ** 2:
-        return '{:.3f} KiB'.format(fsize / 1024)
+        return '{:.3f} KiB'.format(fsize / 1024.0)
 
     if fsize < 1024 ** 3:
-        return '{:.3f} MiB'.format(fsize / (1024 ** 2))
+        return '{:.3f} MiB'.format(fsize / (1024.0 ** 2))
 
-    return '{:.3f} GiB'.format(fsize / (1024 ** 3))
+    return '{:.3f} GiB'.format(fsize / (1024.0 ** 3))
 
 def main(argv0, args):
     if not args:
@@ -36,8 +37,7 @@ def main(argv0, args):
                 raise RuntimeError("{}: no CISO or ZISO 4 magic bytes".format(fname))
 
             filesize = os.path.getsize(fname)
-            origsize = int.from_bytes(data[8:16], 'little')
-            blocksize = int.from_bytes(data[16:20], 'little')
+            origsize, blocksize = struct.unpack('<QI', data[8:20])
             totalsize += filesize
             totalorig += origsize
 
@@ -45,7 +45,7 @@ def main(argv0, args):
                 fname, 'cso' if data[:4] == b'CISO' else 'zso',
                 filesize, origsize,
                 pretty_print_filesize(filesize), pretty_print_filesize(origsize),
-                (100.0 * filesize) / origsize, blocksize
+                (100.0 * filesize) / origsize if origsize else 0, blocksize
             ))
 
         except Exception as e:
